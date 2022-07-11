72f41c9f709eaeb6df6d0c5caae9045def7b9eb5
Hassan Amer <hassan.amer.rr@gmail.com>
2022-07-11 09:04:43 -0300

    respostas 1

---
+++ desafio1.js
@@ -0,0 +1,24 @@
+// RESPOSTAS
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+
+const funcionario2: {codigo: number, nome: string} = {
+    codigo: 10,
+    nome: 'joao'
+}
+
+interface Funcionario {  // Já conhece interfaces? https://blog.logrocket.com/types-vs-interfaces-in-typescript/
+    codigo: number,
+    nome: string
+};
+
+const funcionarioObj = {} as Funcionario;
+funcionarioObj.codigo = 10;
+funcionarioObj.nome = 'João';
+
+const funcionarioObj2: Funcionario = {
+    codigo: 10,
+    nome: 'João'
+}
